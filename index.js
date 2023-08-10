eb6da79395b7147d0e007ccea5374173b8a35826
roo123fre <kibichdaneil@gmail.com>
2023-08-10 12:04:50 +0300

    started creating telegram chat bot

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ const menuItem = document.querySelector('.header__nav-list')
 const portfolio = document.querySelector('.portfolio__information')
 const buttonActive = document.querySelector('.button__active')
 
-menu.addEventListener('click',()=>{
+menu.addEventListener('click', () => {
     menuItem.classList.toggle('active');
 })
-buttonActive.addEventListener('click',()=>{
+buttonActive.addEventListener('click', () => {
     menu.classList.toggle('button__active');
+})
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const name = form.elements.name.value
+    const phone = form.elements.phone.value
+    const message = form.elements.message.value
+
+    const messageMail =`
+    Name: ${name}
+    Phone: ${phone}
+    Message: ${message}
+    `;
+    console.log(messageMail);
+
+
+    const TOKEN = "6690576529:AAEcP9kk4ulyZuWT5U_M2xXfYI8gpRM-pXE";
+    const URI_API="";
+    const ID="";
 })
